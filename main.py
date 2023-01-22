4c99457010be38955ab088f5d091f0aede11850e
Banechka <108561676+666vanechka666@users.noreply.github.com>
2023-01-22 13:39:07 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,152 +1,71 @@
-from sklearn.datasets import load_boston
-import warnings
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-import statsmodels.api as sm
-from sklearn.metrics import mean_squared_error, mean_absolute_error, rand_score
-from sklearn.cluster import KMeans
-import scipy.cluster.hierarchy as sch
 
+#Загружаю датасеты
+ptd1 = pd.read_csv('part_01.csv')
+ptd2 = pd.read_csv('part_02.csv')
+ptd3 = pd.read_csv('part_03.csv')
+ptd4 = pd.read_csv('part_04.csv')
+ptd5 = pd.read_csv('part_05.csv')
+ptd6 = pd.read_csv('part_06.csv')
+ptd7 = pd.read_csv('part_07.csv')
+ptd8 = pd.read_csv('part_08.csv')
+ptd9 = pd.read_csv('part_09.csv')
+ptd10 = pd.read_csv('part_10.csv')
+ptd11 = pd.read_csv('part_11.csv')
+ptd12 = pd.read_csv('part_12.csv')
 
+#Объединяю в один
+btm1 = pd.concat([ptd5, ptd12], ignore_index=True)
+btm2 = pd.concat([ptd6, ptd10], ignore_index=True)
+btm3 = pd.concat([ptd2, ptd11], ignore_index=True)
+btm4 = pd.concat([ptd1, ptd9], ignore_index=True)
+btm5 = pd.concat([ptd3, ptd8], ignore_index=True)
+btm6 = pd.concat([ptd4, ptd7], ignore_index=True)
 
-warnings.filterwarnings("ignore")
-boston = load_boston()
-df = pd.DataFrame(boston.data)
-df.columns = [boston.feature_names]
+final_dataset = btm1.merge(btm2, left_on='ID', right_on='ID')
+final_dataset = final_dataset.merge(btm3, left_on='ID', right_on='ID')
+final_dataset = final_dataset.merge(btm4, left_on='ID', right_on='ID')
+final_dataset = final_dataset.merge(btm5, left_on='ID', right_on='ID')
+final_dataset = final_dataset.merge(btm6, left_on='ID', right_on='ID')
+final_dataset = final_dataset.sort_values(by='ID')
 
-df['PRICE'] = boston.target
-df.isnull()
+#Дропаю дубликаты
+final_dataset = final_dataset.drop_duplicates(keep=False)
 
-# print(df.describe())
-print(df)
-print(df.corr())
+#Первые 10 строк объединенного датасета
+print(final_dataset.head(10))
+#Количество столбцов и колонок в объединенном датасете
+print(final_dataset.shape)
 
-mask = np.zeros_like(df.corr())
-plt.figure(figsize=(16,10))
-sns.heatmap(df.corr(), mask=mask, annot=True, annot_kws={'size': 14})
-sns.set_style('white')
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
-plt.show()
+#Найти в получившемся датасете все поля, которые являются текстовыми
+print(final_dataset.info(verbose=True))
+#Список из названия колонок, где все поля являются текстовыми
+columns_dtype_object = list(final_dataset.select_dtypes(['object']).columns)
+#Привести их значения к нижнему регистру
+for column in columns_dtype_object:
+	final_dataset[column] = final_dataset[column].str.lower()
 
+#Если текстовое значение состоит только из “ “ (пробел), то его заменить на NaN
+for column in columns_dtype_object:
+	final_dataset[column].replace(' ', np.NaN)
 
-
-
-prices = np.log(df['PRICE'])
-features = df.drop('PRICE', axis=1)
-
-X_train, X_test, Y_train, Y_test = train_test_split(features, prices, test_size = 0.2, random_state=10)
-
-
-regr = LinearRegression()
-regr.fit(X_train, Y_train)
-
-print('Коэффициент детерминации(тренировочные данные): ', regr.score(X_train, Y_train))
-print('Коэффициент детерминации(тестовые данные): ', regr.score(X_test, Y_test))
-print('Интерцепт: ', regr.intercept_[0])
-
-
-X_incl_const = sm.add_constant(X_train)
-model = sm.OLS(Y_train, X_incl_const)
-results = model.fit()
-
-reduced_log_mse = round(results.mse_resid, 3)
-
-print('2 СКО в log:', 2 * np.sqrt(reduced_log_mse))
-
-upper_price = np.log(30) + 2 * np.sqrt(reduced_log_mse)
-print('Максимальная цена: $', np.e ** upper_price * 1000)
-
-lower_price = np.log(30) - 2 * np.sqrt(reduced_log_mse)
-print('Минимальная цена: $', np.e ** lower_price * 1000)
-
-
-pred = regr.predict(X_test)
-print('mean_squared_error : ', mean_squared_error(Y_test, pred))
-print('mean_absolute_error : ', mean_absolute_error(Y_test, pred))
-
-
-
-my_dict = {'PRICE': df['PRICE'], 'RM': df['RM']}
-X = pd.DataFrame([my_dict])
-
-X = np.array(X, dtype=float)
-
-wcss = []
-for i in range(1, 11):
-	kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
-	kmeans.fit(X)
-	wcss.append(kmeans.inertia_)
-plt.plot(range(1, 11), wcss)
-plt.title('The Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
-
-
-kmeans = KMeans(n_clusters = 2, init = 'k-means++', random_state = 42)
-y_kmeans = kmeans.fit_predict(X)
-
-df2 = df
-y_kmeans_list = list(y_kmeans)
-
-for i in range(len(y_kmeans_list)):
-	y_kmeans_list[i] += 1
-
-tmp = list(df.iloc[:, 6].values)
-k = 0
-clusters = []
-
-for i in range(len(tmp)):
-	clusters.append(y_kmeans_list[k])
-	k += 1
-
-true_clusters = []
-pricesT = list(df2['PRICE'])
-
-for i in range(len(pricesT)):
-	if float(pricesT[i]) < 21.6:
-		true_clusters.append(2)
-	else:
-		true_clusters.append(1)
-
-df2['Cluster'] = clusters
-df2['True cluster'] = true_clusters
-# df2.to_excel('cluster.xlsx')
-df2.head()
-
-
-
-
-plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'blue', label = 'Cluster 1')
-plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'red', label = 'Cluster 2')
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'black', label = 'Centroids')
-
-plt.title('Clusters')
-plt.xlabel('Price')
-plt.ylabel('RM')
-plt.legend()
-
-plt.show()
-
-X_old = df.iloc[:, [6, 6]].values
-for i in range(len(X_old)):
-	if X_old[i][1] < 7.185:
-		X_old[i] = 0
-X = []
-for i in range(len(X_old)):
-	if sum(X_old[i]) == 0:
+#Если в текстовом значении есть цифры (телефон, дата рождения и др.), то всё значение текстового поля тоже заменить на NaN.
+for column in columns_dtype_object:
+	if column == 'Pack':
 		pass
-	elif sum(X_old[i]) > 0:
-		X.append([X_old[i][0], X_old[i][1]])
-X = np.array(X, dtype=float)
-dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
-plt.title('Dendrogram')
-plt.xlabel('RM')
-plt.ylabel('Euclidean distances')
-plt.show()
+	else:
+		for i in final_dataset[column]:
+			try:
+				final_dataset[column].where((i.isdigit() == True), other=np.NaN, inplace=True)
+			except Exception as e:
+				pass
+
+
+#Выведите количество уникальных значения в полях CLNT_TRUST_RELATION, APP_MARITAL_STATUS, CLNT_JOB_POSITION
+print(len(final_dataset['CLNT_TRUST_RELATION'].unique()))
+print(len(final_dataset['APP_MARITAL_STATUS'].unique()))
+print(len(final_dataset['CLNT_JOB_POSITION'].unique()))
+
+#Выведите колонки итогового датасета
+print(final_dataset.columns)
